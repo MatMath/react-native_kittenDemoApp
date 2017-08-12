94d0d24e2672acd88e4c7f7f7cdefe1e1aec5a66
MatMath <mathieu.k.legault@gmail.com>
2017-08-12 14:32:19 -0400

    Adding a slider to display different images Moving the style into his own file.

--- src/app.js
+++ src/app.js
@@ -5,47 +5,65 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, Linking } from 'react-native';
+import { Text, View, Linking, Image, Slider } from 'react-native';
 
-const kitten1 = require('./img/kitten_1.png');
+// local Code
+import { styles } from './styles';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+// Images list
+const img0 = require('./img/kitten_0.png');
+const img1 = require('./img/kitten_1.png');
+const img2 = require('./img/kitten_2.png');
+const img3 = require('./img/kitten_3.png');
+const img4 = require('./img/kitten_4.png');
+
+const kittenList = [img0, img1, img2, img3, img4];
+const wtfJs = () => (0.1 + 0.2);
 
 class kittenDemo extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: 'Welcome to the Kitten App!',
+      displayKitten: 0,
+      wftjs: wtfJs(),
     };
+    this.changeKitten = this.changeKitten.bind(this);
+  }
+  changeKitten(nbr) {
+    this.setState({ displayKitten: nbr });
   }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          {this.state.title}
-        </Text>
-        <Image source={kitten1} />
-        <Text
-          style={{ color: 'blue' }}
-          onPress={() => Linking.openURL('http://placekitten.com/attribution.html')}>
-          Images from PlaceKitten
-        </Text>
+        <View style={styles.topSection}>
+          <Text style={styles.welcome}>
+            {this.state.title}
+          </Text>
+          <Text>{this.state.wftjs}</Text>
+        </View>
+        <View style={styles.coreSection}>
+          <Slider
+            minimumValue={0}
+            maximumValue={4}
+            step={1}
+            value={this.state.displayKitten}
+            onValueChange={this.changeKitten}
+            style={{ width: 200 }}
+          />
+          <Image source={kittenList[this.state.displayKitten]} />
+        </View>
+        <View style={styles.bottomSection}>
+          <Text
+            style={{ color: 'blue' }}
+            onPress={() => Linking.openURL('http://placekitten.com/attribution.html')}
+          >
+            Images from PlaceKitten
+          </Text>
+        </View>
       </View>
     );
   }
 }
-
 
 module.exports.kittenDemo = kittenDemo;
