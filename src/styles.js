bf1ea30c4f8ffbdb5cbfc23535c78abf615cccf2
MatMath <mathieu.k.legault@gmail.com>
2017-08-12 20:44:45 -0400

    Adding more styles to center thing.

--- src/styles.js
+++ src/styles.js
@@ -1,16 +1,31 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
     margin: 10,
+    justifyContent: 'center',
+  },
+  topSection: {
+    flex: 1,
+    alignContent: 'center',
+    alignSelf: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'blue',
+  },
+  coreSection: {
+    flex: 8,
+    backgroundColor: 'grey',
+  },
+  bottomSection: {
+    flex: 0.5,
+    backgroundColor: 'pink',
   },
 });
 
